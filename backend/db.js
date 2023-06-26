6f3ef39cdb0ff8384816e433d0ba497a4d00270c
Shubhanker <shubhankertiwari8@gmail.com>
2023-06-26 16:58:39 +0530

    database connected

---
+++ backend/db.js
@@ -0,0 +1,22 @@
+
+
+const mongoose = require('mongoose');
+
+
+const URL ='mongodb://127.0.0.1/e-feedbackDB';
+
+
+const connectToMongo = ()=>{
+    mongoose.connect(URL,  {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("DB Connection Successful");
+    })
+    .catch((err) => {
+        console.log(err.message);
+    })
+}
+
+module.exports = connectToMongo;

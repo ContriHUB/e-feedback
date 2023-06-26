11318bb585950b2fc1f8a194e6d0aa5cf0170c4f
Shubhanker <shubhankertiwari8@gmail.com>
2023-06-26 18:16:25 +0530

    data added

--- backend/db.js
+++ backend/db.js
@@ -19,4 +19,5 @@ const connectToMongo = ()=>{
     })
 }
 
+
 module.exports = connectToMongo;
--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,12 @@ connectToMongo();
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 const port =3000
 
 app.use('/api/auth',require('./routes/auth'))
-//app.use('/api/notes',require('./routes/notes'))
+app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port, () =>{
     console.log(`Example app listening at http://localhost:${port}`)
